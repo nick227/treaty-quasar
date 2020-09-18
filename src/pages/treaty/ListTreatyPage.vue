ae54534822b440d5e5697ff3a1060a752d4b69e1
Nicholas Rios <nicholas.jay.rios@gmail.com>
2020-09-18 06:00:44 -0500

    adding to treaty list

--- src/pages/treaty/ListTreatyPage.vue
+++ src/pages/treaty/ListTreatyPage.vue
@@ -6,12 +6,7 @@
     <q-list padding>
       <q-separator />
     <div v-for="treaty in treaties" :key="treaty.id">
-          <div class="row q-pl-md q-pr-md" style="">
-            <div class="col col-5"><h6>org a</h6></div>
-            <div class="col col-2 text-center"><h6>vs.</h6></div>
-            <div class="col col-5 text-right"><h6>org b</h6></div>
-          </div>
-      <q-item class="full-width" tag="a" :to="'/treaty/'+treaty.id">
+      <q-item class="full-width" tag="a" :to="'/conflict/'+treaty.conflict_id">
          <q-card class="full-width">
       <q-img rounded class="q-mt-none" :src="treaty.avatar_url"></q-img>
       <q-card-section>
@@ -20,6 +15,19 @@
       <q-card-section class="q-pt-none">
         {{ treaty.description }}
       </q-card-section>
+          <div class="row q-pt-none q-pl-md q-pr-md" style="">
+            <div class="col">Created by: {{ treaty.creator_name }}</div><div class="col text-right">Organization: {{ treaty.creator_organization_name }}</div>
+          </div>
+          <q-separator />
+          <div class="row q-pl-md q-pr-md" style="">
+            <div class="col col-5"><h6>{{ treaty.organization_a.name }}</h6></div>
+            <div class="col col-2 text-center"><h6>vs.</h6></div>
+            <div class="col col-5 text-right"><h6>{{ treaty.organization_b.name }}</h6></div>
+          </div>
+          <q-separator />
+          <div class="row q-pa-md" style="">
+            <div class="col">{{ treaty.grievances.length }} Grievances | {{ treaty.offers.length }} Offers</div><div class="col text-center">Rated {{ treaty.rating }}/5</div><div class="col text-right">Votes ~ Yay: {{ treaty.yay_votes }} Nay: {{ treaty.nay_votes }}</div>
+          </div>
     </q-card>
       </q-item>
       <q-separator />
@@ -57,12 +65,31 @@ export default {
       this.loadNum++
     },
     loadTreaties: async function () {
-      const q = `${process.env.api}/treaties?filter[order]=create_date%20DESC&filter[limit]=${this.limit}&filter[skip]=${this.pointer}`
+      const q = `${process.env.api}/treaties?filter={"skip":${this.pointer},"limit":${this.limit},"order":["create_date DESC"], "include": [{"relation": "organization"}, {"relation":"creator"}, {"relation": "conflict", "scope":{"include":[{"relation":"offers"}, {"relation":"grievances"}, {"relation":"organization_a"}, {"relation":"organization_b"}]}}, {"relation":"votes"}, {"relation": "ratings"}]}`
       const treaties = await this.$axios.get(q)
       if (this.limit > treaties.data.length) {
         this.done = true
       }
-      this.treaties = this.treaties.concat(treaties.data)
+      const obj = treaties.data.map((o) => {
+        return {
+          name: o.name,
+          description: o.description,
+          avatar_url: o.avatar_url,
+          creator_name: o.creator.name,
+          creator_organization_name: o.organization.name,
+          conflict_id: o.conflict.id,
+          organization_a: o.conflict.organization_a,
+          organization_b: o.conflict.organization_b,
+          grievances: o.conflict.grievances,
+          offers: o.conflict.offers,
+          yay_votes: o.votes.filter(function (elm) { return elm.vote_type === 1 }).reduce((total, next) => total + next.vote_type, 0),
+          nay_votes: o.votes.filter(function (elm) { return elm.vote_type === 0 }).reduce((total, next) => total + next.vote_type, 0),
+          votes: o.votes,
+          rating: Math.round(o.ratings.reduce((total, next) => total + next.value, 0) / o.ratings.length)
+        }
+      })
+      this.treaties = this.treaties.concat(obj)
+      console.log(this.treaties)
     }
   },
   async created () {
